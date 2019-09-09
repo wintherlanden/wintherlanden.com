79334dd6117d45360212b733cbadf51760fa7bde
root <root@mail.wintherlanden.com>
2019-09-09 14:37:01 +0200

    .

--- head.inc.php
+++ head.inc.php
@@ -2,11 +2,27 @@
 <!DOCTYPE html>
 <html lang="de-DE">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="profile" href="https://gmpg.org/xfn/11">
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 
 <title>Wintherlanden</title>
+
+
+<style>
+body {
+    background: black;
+    color: white;
+}
+h1, h2, h3, h4, p, body {
+    font-family: 'Montserrat', sans-serif;
+
+}
+</style>
+
 
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 
 require_once('head.inc.php');
 
-echo $_REQUEST['uri'];
+
+$page = $_REQUEST['uri'];
+
+if ($page[0] == '/') {
+    $page = substr($page, 1);
+}
+if (!$page) $page = 'home';
+require_once($page . '.html');
 
 
 require_once('foot.inc.php');
