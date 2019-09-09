f5809f8029c80d74b6c81a1a90a722d7ffef5d52
Melina Schuler <info@wintherlanden.com>
2019-09-09 13:25:10 +0200

    .

---
+++ head.inc.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="de-DE">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+<title>Wintherlanden</title>
+
+</head>
+<body>
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+require_once('head.inc.php');
+
+echo $_REQUEST['uri'];
+
+
+require_once('foot.inc.php');
