7219bfdff37601959e78303a1a5b10f1e252e6be
root <root@mail.wintherlanden.com>
2019-09-24 10:40:01 +0200

    .

--- head.inc.php
+++ head.inc.php
@@ -31,10 +31,10 @@ background-position: center center;
    text-transform: uppercase;
 
 }
-.navbar li a {
+.navbar li a.nav-link {
     font-size: 15px;
     letter-spacing: 1px;
-    font-weight: 600;
+    font-weight: 600 !important;
 }
 </style>
 
@@ -45,7 +45,7 @@ background-position: center center;
 <div id='header_logo'>
 </div>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
