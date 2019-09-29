af2f5a52fa232ab52bdec1ba750573f228a86616
root <root@mail.wintherlanden.com>
2019-09-29 19:29:01 +0200

    .

--- thumb.php
+++ thumb.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 $src = __DIR__ . '/' . $_GET['src'];
 
 $desired_width = 300;
@@ -8,16 +12,17 @@ $pInfo = pathinfo($src);
 // Save the new path using the current file name
 $dest_dir = __DIR__ . '/thumbnails/';
 @mkdir($dest_dir);
-$dest_filename = $pInfo['basename'] . '.' . $desired_width;
+$dest_filename = $pInfo['basename'] . '.' . $desired_width . '.png';
 $dest = $dest_dir . $dest_filename;
 // Do the rest of your stuff and things...
-$source_image = imagecreatefromjpeg($src);
+$source_image = @imagecreatefromjpeg($src);
+if (!$source_image) $source_image = imagecreatefrompng($src);
 $width = imagesx($source_image);
 $height = imagesy($source_image);
 $desired_height = floor($height * ($desired_width / $width));
 $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
 imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
-imagejpeg($virtual_image, $dest);
+imagepng($virtual_image, $dest);
 
 
 if (is_file($dest)) {
