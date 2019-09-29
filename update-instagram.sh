6c8697799dc5378577e1a95b5383f53aae24368c
root <root@mail.wintherlanden.com>
2019-09-29 17:44:01 +0200

    .

--- update-instagram.sh
+++ update-instagram.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl https://www.instagram.com/fineart_wintherlanden/ | grep "_sharedData =" > instagram.inc.php
+curl https://www.instagram.com/fineart_wintherlanden/ | grep "_sharedData =" > /tmp/.instagram-update
+grep _sharedData /tmp/.instagram-update && mv /tmp/.instagram-update instagram.inc.php
 
