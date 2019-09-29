96fe48087a4f40722e8022a505b19d754fb7aff3
root <root@mail.wintherlanden.com>
2019-09-29 15:24:01 +0200

    .

--- head.inc.php
+++ head.inc.php
@@ -44,9 +44,9 @@ background-position: center center;
     height: 20px;
     z-index: 99999;
 
-    -webkit-box-shadow: 0 8px 6px -6px #999;
-    -moz-box-shadow: 0 8px 6px -6px #999;
-    box-shadow: 0 8px 6px -6px #999;
+    -webkit-box-shadow: 0 -8px 6px -6px #999;
+    -moz-box-shadow: 0 -8px 6px -6px #999;
+    box-shadow: 0 -8px 6px -6px #999;
 }
 </style>
 
