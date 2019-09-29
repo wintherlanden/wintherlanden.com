102e29d97fccf4e597c8a6a86477ecb5fcadc890
root <root@mail.wintherlanden.com>
2019-09-29 19:23:01 +0200

    .

--- thumb.php
+++ thumb.php
@@ -1,1 +1,16 @@
 <?php
+
+$src = $_GET['src'];
+
+$pInfo  =   pathinfo($src);
+// Save the new path using the current file name
+$dest   =   $dest."/".$pInfo['basename'];
+// Do the rest of your stuff and things...
+$source_image = imagecreatefromjpeg($src);
+$width = imagesx($source_image);
+$height = imagesy($source_image);
+$desired_height = floor($height * ($desired_width / $width));
+$virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+imagejpeg($virtual_image, $dest);
+
