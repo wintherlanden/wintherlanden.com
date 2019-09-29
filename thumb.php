0f29f834e88f9b3192e844ee6d3613cb6014fe45
root <root@mail.wintherlanden.com>
2019-09-29 19:25:01 +0200

    .

--- thumb.php
+++ thumb.php
@@ -1,10 +1,13 @@
 <?php
 
-$src = $_GET['src'];
+$src = __DIR__ . '/' . $_GET['src'];
 
-$pInfo  =   pathinfo($src);
+$desired_width = 300;
+
+$pInfo = pathinfo($src);
 // Save the new path using the current file name
-$dest   =   $dest."/".$pInfo['basename'];
+$dest_filename = $pInfo['basename'] . '.' . $desired_width;
+$dest = __DIR__ . '/thumbnails/' . $dest_filename;
 // Do the rest of your stuff and things...
 $source_image = imagecreatefromjpeg($src);
 $width = imagesx($source_image);
@@ -14,3 +17,6 @@ $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
 imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
 imagejpeg($virtual_image, $dest);
 
+
+header('Location: /thumbnails/' . $dest_filename);
+exit;
