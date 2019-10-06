bc370e57e99b34b2545442a67e9de52f0949f437
root <root@mail.wintherlanden.com>
2019-10-06 15:36:01 +0200

    .

--- head.inc.php
+++ head.inc.php
@@ -122,6 +122,7 @@
             background: rgba(129, 139, 160, 1.0) !important;
         }
         #cookie-bar-main-privacy-link {
+            color: rgba(255, 255, 255, 1) !important;
         }
     </style>
 
