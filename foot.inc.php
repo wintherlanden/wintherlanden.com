20406fe393a4dd5431e0e1ebd7c3b76ab74db9bc
root <root@mail.wintherlanden.com>
2019-10-06 15:00:02 +0200

    .

--- foot.inc.php
+++ foot.inc.php
@@ -1,99 +1,101 @@
-
 <?php
 if (!$nolayout) {
     ?>
-    
+
     </div>
 
-<style>
-    /* Sticky footer styles
-    -------------------------------------------------- */
-    html {
-        position: relative;
-        min-height: 100%;
-    }
+    <style>
+        /* Sticky footer styles
+        -------------------------------------------------- */
+        html {
+            position: relative;
+            min-height: 100%;
+        }
 
-    body {
-        margin-bottom: 38px; /* Margin bottom by footer height */
-    }
+        body {
+            margin-bottom: 38px; /* Margin bottom by footer height */
+        }
 
-    .footer {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        height: 38px; /* Set the fixed height of the footer here */
-        line-height: 38px; /* Vertically center the text there */
-        background: rgba(113, 130, 146, 0.63);
-    }
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 38px; /* Set the fixed height of the footer here */
+            line-height: 38px; /* Vertically center the text there */
+            background: rgba(113, 130, 146, 0.63);
+        }
 
-    .footer .text-muted {
-        text-align: right;
-    }
+        .footer .text-muted {
+            text-align: right;
+        }
 
-    .footer a {
-        color: rgba(255, 255, 255, 0.63);
-        margin-left: 20px;
-    }
-</style>
+        .footer a {
+            color: rgba(255, 255, 255, 0.63);
+            margin-left: 20px;
+        }
+    </style>
 
-<footer class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="text-muted">
-                    <a href="?datenschutz">Datenschutz</a>
-                    <a href="?impressum">Impressum</a>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="text-muted">
+                        <a href="?datenschutz">Datenschutz</a>
+                        <a href="?impressum">Impressum</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-<script src="/ekko-lightbox.js?version=2"
-        crossorigin="anonymous"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    <script src="/ekko-lightbox.js?version=2"
+            crossorigin="anonymous"></script>
 
-<script>
+    <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?theme=flying&thirdparty=1&always=1&refreshPage=1&hideDetailsBtn=1&showPolicyLink=1&remember=40&privacyPage=https%3A%2F%2Fwww.wintherlanden.com%2F%3Fdatenschutz"></script>
 
-    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
+    <script>
 
-    $(function () {
-        $('#button_lang_de').click(function () {
-            $.post(
-                '/set-language.php?language=de',
-                function () {
-                    window.location.reload();
-                }
-            );
+        $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
         });
-        $('#button_lang_en').click(function () {
-            $.post(
-                '/set-language.php?language=en',
-                function () {
-                    window.location.reload();
-                }
-            );
+
+        $(function () {
+            $('#button_lang_de').click(function () {
+                $.post(
+                    '/set-language.php?language=de',
+                    function () {
+                        window.location.reload();
+                    }
+                );
+            });
+            $('#button_lang_en').click(function () {
+                $.post(
+                    '/set-language.php?language=en',
+                    function () {
+                        window.location.reload();
+                    }
+                );
+            });
         });
-    });
 
-</script>
+    </script>
 
 
-<?php
+    <?php
 }
 ?>
 
