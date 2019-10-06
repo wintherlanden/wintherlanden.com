d205d553f857cb468b0a247b9a8df15d7bd0a12d
root <root@mail.wintherlanden.com>
2019-10-06 17:42:01 +0200

    .

--- devices.php
+++ devices.php
@@ -1,5 +1,11 @@
 <?php
 
+define('LN', "\n");
+header("X-Frame-Options: allow-from https://www.instagram.com/");
+header("Access-Control-Allow-Origin: *");
+header("Content-Security-Policy: frame-ancestors https://instagram.com https://www.wintherlanden.com");
+session_start();
+
 $devices = [
     'Desktop' => [
         'width' => 1200,
